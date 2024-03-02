ae1e5494fab90fb495acd75df04e940c65f15573
ZarinaNe <93059396+ZarinaNe@users.noreply.github.com>
2024-03-02 08:49:23 +0300

    Update lab6.cpp

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -6,13 +6,20 @@
 
 using namespace std;
 
+/*Шаблон "Фабрика" (Factory) - это паттерн проектирования, который предоставляет общий интерфейс для создания объектов, но позволяет подклассам 
+решать, какой класс конкретного объекта следует создать. Это позволяет делегировать создание объектов подклассам, таким образом, обеспечивая 
+более гибкую архитектуру и упрощая добавление новых типов объектов без изменения существующего кода.*/
+
 int main()
 {
-    Factory *factory = new Factory();
+    Factory *factory = new Factory(); //создает указатель на объект типа Factory
+    //Цикл выбора операционной системы:
     int os = -1;
     while (os != 0) {
         cout << "Choose your OS\nWindows - 1\nLinux - 2\nMacOS - 3\nExit - 0\n";
         cin >> os;
+        //В зависимости от выбранной операционной системы создается объект соответствующей фабрики.
+        //Объект этой фабрики затем используется для создания элементов управления.
         switch (os) {
         case 1:
             factory = (Factory*)new WinFactory();
@@ -24,11 +31,14 @@ int main()
             factory = (Factory*)new MacFactory();
             break;
         }
-        factory->createButton()->click();
-        factory->createComboBox()->getItems();
-        factory->createForm()->addControl();
-        factory->createLabel()->getText();
-        factory->createTextBox()->getText();
+        /*Создание элементов управления с использованием выбранной фабрики:
+        Вызывается метод createButton() на объекте фабрики factory, который создает объект Button и возвращает указатель на него.
+        на созданном объекте Button вызывается метод click(), который выполняет необходимое действие*/
+        factory->createButton()->click(); 
+        factory->createComboBox()->getItems(); //создается комбинированный список, получается список его элементов
+        factory->createForm()->addControl();   //создается форма, добавляется управляющий элемент (Control) к этой форме
+        factory->createLabel()->getText();     //создается метка (Label),получается текст, который отображается на этой метке
+        factory->createTextBox()->getText();   //создается текстовое поле, получается текст, введенный в это текстовое поле
         cout << endl << endl;
     }
 }
